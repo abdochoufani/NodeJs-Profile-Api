e9fb6aa7fbf7a25de0dec08df9de215020a492b3
abdo choufani <abdo.elchoufani@gmai.com>
2019-05-14 17:43:17 +0200

    adding layout components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Landing from './components/layout/Landing'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
+
 function App() {
   return (
     <div className="App">
-      <h1>My app</h1>
+      <Navbar />
+      <Landing />
+      <Footer />
     </div>
   );
 }
